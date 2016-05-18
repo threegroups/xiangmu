e5a081b16d7fa467e65dd3b0bca53264dffb8336
maojianyou <15210301442@163.com>
2016-05-18 09:39:43 +0800

    56326532

--- yii2/frontend/controllers/IndexController.php
+++ yii2/frontend/controllers/IndexController.php
@@ -32,10 +32,10 @@ class IndexController extends Controller{
     {
         return $this->render('product-list.html');
     }
-    public function actionFeedback_list()
+    /*public function actionFeedback_list()
     {
         return $this->render('feedback-list.html');
-    }
+    }*/
     public function actionMember_list()
     {
         return $this->render('member-list.html');
